e5ede8cc38da3f8e5430a58c2483fbc4c82e1038
DulongSang <dulong@ualberta.ca>
2020-06-22 00:10:25 -0600

    feat: navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
-      Hello World!
+      <Navbar />
     </div>
   );
 }

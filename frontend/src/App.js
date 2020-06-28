4c73d24863fe9d8c0992d436050bb0963abfb49b
DulongSang <dulong@ualberta.ca>
2020-06-27 22:11:47 -0600

    change: add login page to react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
+import Login from "./components/Login";
 
 function App() {
   return (
     <Router>
-    <div>
-      <Navbar />
-      <div className="flex" style={{height: "90vh"}}>
-        <Sidebar />
-        <div className="main-container">
-          <Route path="/chat" component={Chat} exact/>
+      <Route exact path="/login" component={Login} />
+      <Route path="/app">
+        <div>
+          <Navbar />
+          <div className="flex" style={{height: "90vh"}}>
+            <Sidebar />
+            <div className="main-container">
+              <Route exact path="/chat" component={Chat}/>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </Route>
     </Router>
   );
 }
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import { EyeSlash, Eye, ExclamationCircleFill } from "react-bootstrap-icons";
+import "../styles/register.css";
+import logo from "../images/unicorn.png";
+import config from "../config.json";
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showPwd: false,
+      username: "",
+      password: "",
+      remember: true,
+      errorInfo: null
+    };
+
+    this.showHidePassword = this.showHidePassword.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  showHidePassword() {
+    this.setState(prevState => ({showPwd: !prevState.showPwd }));
+  }
+
+  handleChange(event) {
+    const name = event.target.name;
+    if (name === "remember") {
+      this.setState({ [name]: event.target.checked });
+    } else {
+      this.setState({ [name]: event.target.value });
+    }
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const loginInfo = { 
+      username: this.state.username,
+      password: this.state.password,
+      remember: this.state.remember
+    };
+
+    const http = new XMLHttpRequest();
+    const url = config.hostOrigin + "/api/user/auth";
+    http.open("POST", url, true);
+    http.setRequestHeader("Content-Type", "application/json");
+
+    http.onreadystatechange = () => {
+      // state 4: done
+      if (http.readyState !== 4) {
+        return;
+      }
+
+      if (http.status === 200) {
+        const response = JSON.parse(http.responseText);
+      } else {
+        this.setState({ errorInfo: http.responseText });
+      }
+    };
+    http.send(JSON.stringify(loginInfo));
+  }
+
+  render() {
+    return (
+      <div className="login-container">
+        <form className="login-form">
+          <img src={logo} alt="unicorn" width="60px" height="60px" />
+          <h1>NSNMN Login</h1>
+
+          <div className="textb">
+            <input type="text" name="username" autoComplete="off" spellCheck="false"
+              value={this.state.username} onChange={this.handleChange} required />
+            <div className="placeholder">Username</div>
+          </div>
+          
+          <div className="textb">
+            <input type={this.state.showPwd ? "text" : "password"} name="password" 
+              value={this.state.password} onChange={this.handleChange} required/>
+            <div className="placeholder">Password</div>
+            <div style={{position: "absolute", top: "20%", right: "5%",
+              color: "#9d9d9d", fontSize: "26px", cursor: "pointer"}}
+              onClick={this.showHidePassword} >
+            {this.state.showPwd ? <Eye /> : <EyeSlash />}
+            </div>
+          </div>
+
+          <div className={`info-container ${this.state.errorInfo ? "": "hide"}`}>
+            <ExclamationCircleFill style={{marginRight: "6px", bottom: "0.1em", position: "relative"}} />
+            <span id="info-msg">{this.state.errorInfo}</span>
+          </div>
+          <input className="login-btn" type="submit" value="Login" onClick={this.handleSubmit} />
+          <div className="checkbox">
+            <input type="checkbox" name="remember" 
+              checked={this.state.remember} onChange={this.handleChange} />
+            Remember me
+          </div>
+          <div style={{marginTop: "20px"}}>
+            <span>Don't have an account?</span><a href="register.html">Create one</a>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}

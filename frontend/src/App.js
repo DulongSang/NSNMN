d6902c25eeeb5dd62c22ce80075c02d733f837bb
DulongSang <dulong@ualberta.ca>
2020-06-28 20:11:30 -0600

    feat: add home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/register.css";
+import "./styles/mainPage.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
@@ -9,10 +10,12 @@ import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import MainPage from "./components/MainPage";
 
 function App() {
   return (
     <Router>
+      <Route exact path="/" component={MainPage} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
       <Route path="/app">
---
+++ frontend/src/components/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function MainPage() {
+    return (
+        <div>
+            <h1 className="main-title">Black Lives Matter!</h1>
+            <h1 className="brand">NSNMN</h1>
+            <a href="/login">Login</a>
+            <a href="/register">Sign up</a>
+        </div>
+    )
+}
